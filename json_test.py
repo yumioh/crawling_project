3458f85a87b41bfbf80798a3e10d39222d2c6f8b
spark <spark.dev@gmail.com>
2023-09-19 12:46:08 +0900

    Tested on reactions jquery for all the 3 news sections

--- json_test.py
+++ json_test.py
@@ -9,8 +9,13 @@ enter_url='https://news.like.naver.com/v1/search/contents?suppress_response_code
 sports_url ='https://sports.like.naver.com/v1/search/contents?suppress_response_codes=true&callback=jQuery111308237307660858897_1695089718681&q=SPORTS%5Bne_001_0014105967%5D%7CJOURNALIST%5B27942(period)%5D%7CSPORTS_MAIN%5Bne_001_0014105967%5D&isDuplication=false&cssIds=MULTI_PC%2CSPORTS_PC&_=1695089718682'
 news_url = 'https://news.like.naver.com/v1/search/contents?suppress_response_codes=true&callback=jQuery3310999753245169029_1695089763056&q=NEWS%5Bne_001_0014105964%5D&isDuplication=false&cssIds=MULTI_MOBILE%2CNEWS_MOBILE&_=1695089763057'
 
-react_response = requests.get(sports_url,headers=headers)
-react_json = re.findall('(?<=jQuery.{34}\()(.*)(?=\);)', react_response.text)[0]
+#url = enter_url
+#url = sports_url
+url = news_url
+react_response = requests.get(url,headers=headers)
+callback = re.findall('(?<=callback\=)(.+?)(?=&)', url)[0]
+contents_id = re.findall('(?<=%5B)(.+?)(?=%5D)', url)[0]
+react_json = re.findall(f'(?<={callback}\()(.+)(?=\);)', react_response.text)[0]
 #len(react_json)
 #print(react_json)
 json_data = json.loads(react_json)
@@ -18,7 +23,7 @@ json_data = json.loads(react_json)
 
 _reactions = {}
 for data in json_data['contents']:
-    if data['serviceId'] == 'SPORTS_MAIN': # 연예 ENTERTAIN_MAIN 스포츠 SPORTS_MAIN 일반
+    if data['contentsId'] == contents_id: # 연예 ENTERTAIN_MAIN 스포츠 SPORTS_MAIN 일반
         for reaction in data['reactions']:
             key = reaction['reactionType']
             val = reaction['count']
