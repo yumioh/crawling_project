d783b309457b4ea8cd8f9227645fba1b1364f745
spark <spark.dev@gmail.com>
2023-09-19 14:56:05 +0900

    Add naver_sprots_crawling test

---
+++ naver_sports_crawling.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+article_url = 'https://sports.news.naver.com/news?oid=445&aid=0000143530'
+headers = {'User-Agent':'Mozila/5.0'}
+response = requests.get(article_url, headers=headers)
+html = response.text
+soup = bs(html, 'html.parser')
+
+title = soup.select_one('h4.title')
+content = soup.select_one('#newsEndContents')
+# delete 'div', 'p' tags in the content
+divs = content.select('div')
+for div in divs:
+    div.decompose()
+ps = content.select('p')
+for p in ps:
+    p.decompose()
+
+print(title.text)
+print(content.text)
+
+
+
+
+
+
+
+
