93827b28b75299eb2b7fc26df0971ad7f85492e0
yumi oh <13yooms@gmail.com>
2023-09-28 16:15:05 +0900

    태그 제거 테스트 파일 수정

--- test/extract_tags.py
+++ test/extract_tags.py
@@ -8,21 +8,85 @@ headers = {
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
 }
 
-newsList_response = requests.get("https://n.news.naver.com/mnews/article/001/0014164627", headers=headers)
+url = 'https://n.news.naver.com/mnews/article/001/0014164610'
+sport_url = 'https://sports.naver.com/news?oid=001&aid=0014164596'
+enter_url = 'https://entertain.naver.com/read?oid=001&aid=0014164167'
+
+newsList_response = requests.get(enter_url, headers=headers)
 newsList_bs = BeautifulSoup(newsList_response.text, 'html.parser')
 
+# #전처리를 위한 정규식
+# def content_reg(content):
+        
+#     email_reg = '[\w.+-]+@[\w-]+\.[\w.-]+' #이메일주소
+#     em_reg = '<em*(.*?)<(\/?)em>' #em태그
+#     br_reg = '(<([^>]+)>)' # 모든태그 없애기
+#     button_reg = '<button*(.*?)<(\/?)button>' #버튼태그
+#     dt_reg = '<dt*(.*?)<(\/?)dt>' #dt태그
+#     ap_tag= '<[a|p](.*)<(\/?)[a|p]>'#a/p 태그
+
+#     content= content.replace('\n', '')
+#     result = re.sub(email_reg,'',content)
+#     result = re.sub(ap_tag,'',result)
+#     result = re.sub(em_reg,'',result)
+#     result = re.sub(button_reg,'',result)
+#     result = re.sub(dt_reg,'',result)
+#     result = re.sub(br_reg,'',result)
+#     return result
+
+def email_reg(content) -> str:
+    result = re.sub('[\w.+-]+@[\w-]+\.[\w.-]+', '', content)
+    return result
+
 #일반 신문 태그 제거 
-def extractTag(text) :
-    text.select_one('strong').decompose()
-    for value in text.select('span.end_photo_org') :
-        value.select_one('em').decompose()
-    content = text.getText().split('=')[2]
+def extractTags(text) :
+    content = ''
+    if text.select_one('strong') :
+       text.select_one('strong').decompose()
+    if text.select_one('span.end_photo_org > em') :
+       for value in text.select('span.end_photo_org') :
+           value.select_one('em').decompose()
+    if len(text.getText().split('=')) != 1 : # 영문신문인 경우 = 이 없음
+       content = text.getText().split('=')[2]
+    else : 
+        content = text.getText()
+    result = re.sub(' +', ' ', content)
+    return(email_reg(result.strip()))
+
+def sports_extractTags(text) :
+    for pTag in text.select('p') :
+        pTag.decompose()
+    for divTag in text.select('div'):
+        divTag.decompose()
+    if text.select_one('span.end_photo_org em') :
+        for emTag in text.select('span.end_photo_org') :
+            emTag.select_one('em').decompose()
+    content = text.getText().split('연합뉴스)')[1]
     result = re.sub(' +', ' ', content)
     return(result)
 
+def enter_cleanup_content(text) :
+    content = ''
+    if text.select_one('strong') :
+       text.select_one('strong').decompose()
+    if text.select_one('span.end_photo_org > em') :
+       for value in text.select('span.end_photo_org') :
+           value.select_one('em').decompose()
+    if len(text.getText().split('=')) == 1 : # 영문신문인 경우 = 이 없음
+       content = text.getText()
+    elif len(text.getText().split('=')) == 2:
+       content = text.getText().split('연합뉴스)')[1] #기자명이 없는 경우
+    else :
+        content = text.getText().split('=')[2]
+    result = re.sub(' +', ' ', content)
+    return(email_reg(result.replace('\n','').strip()))
+
 #extractTag(newsList_bs)
 text = newsList_bs.select_one('article#dic_area')
-print(extractTag(text))
+#newsList_bs.p.decompose()
+sports_text = newsList_bs.select_one('div#newsEndContents')
+enter_text = newsList_bs.select_one('#articeBody')
+print(enter_cleanup_content(enter_text))
 #print(contents.select_one('strong').decompose())
 #print(contents)
 #extractTags(contents)
