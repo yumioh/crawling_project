4032d76cb1a0a438053735f9726decdad04073cf
yumi oh <13yooms@gmail.com>
2023-10-09 03:12:44 +0900

    날짜 형식 변경

--- Tesla_Project/teslaNewsData.py
+++ Tesla_Project/teslaNewsData.py
@@ -4,6 +4,7 @@ import requests
 import json
 import pandas as pd
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
@@ -17,6 +18,27 @@ def email_reg(content) -> str:
 
 def clenaup_content(text) :
   return 0
+
+#날짜형식 변경
+def remove_time_prefix(input_str) :
+  if "오후" or "오전" in input_str :
+    print(input_str)
+    #기사입력, 오후, 오전 반환
+    date_time_str = re.sub(r'기사입력|오후 |오전 ', '', input_str).strip()
+    #2023.10.07. 05:21
+    try :
+        date_time = datetime.strptime(date_time_str,"%Y.%m.%d. %H:%M")
+    except ValueError: #올바른 형식으로 파싱 할수 없는 경우 None 반환
+      return None
+    
+    #시간을 24시간 형식으로 변경 
+    if "오후" in input_str:
+      date_time = date_time.replace(hour=date_time.hour + 12)
+    #datetime을 문자열로 출력
+    formatted_date_time = date_time.strftime("%Y-%m-%d %H:%M:%S")
+  else :
+      formatted_date_time = input_str
+  return formatted_date_time
 
 #네이버 뉴스기사 정보 가져오기 
 def get_news(URL) :
@@ -68,14 +90,16 @@ def get_news(URL) :
     content = re.sub(r'[\r\n\t\"\'\,]', '', content)
   else:
     content = "No Content Found"
-  return (title, date, media, email_reg(content), URL)
+  return (title, remove_time_prefix(date), media, email_reg(content), URL)
 
 #뉴스 리스트 들고 오기
 def get_news_list(keyword, toDate, fromDate) :
     news = []
     page = "1" #뉴스 리스트 페이지
 
-    URL = 'https://search.naver.com/search.naver?where=news&sm=tab_pge&query='+ keyword +'&sort=1&photo=0&field=0&pd=3&ds='+ toDate +'&de='+ fromDate +'&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:dd,p:from'+ fromDate.replace(".","") +'to'+ toDate.replace(".","")+',a:all&start='+page
+    #TODO : 페이징 처리
+
+    URL = 'https://search.naver.com/search.naver?where=news&sm=tab_pge&query='+ keyword +'&sort=1&photo=0&field=0&pd=3&ds='+toDate+'&de='+fromDate+'&mynews=0&office_type=0&office_section_code=0&news_office_checked=&office_category=0&service_area=0&nso=so:dd,p:from'+fromDate.replace(".","")+'to'+toDate.replace(".","")+',a:all&start='+page
     print(URL)
     res = requests.get(URL,headers = headers)
     soup = BeautifulSoup(res.text, "html.parser")
