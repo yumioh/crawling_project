a5a313cc06068b26a4aa4b409fe7ab5e5d04dd67
yumi oh <iove0103@naver.com>
2024-02-05 18:33:29 +0900

    시각화 수정

--- Tesla_Project/7_tesla_fve.py
+++ Tesla_Project/7_tesla_fve.py
@@ -4,8 +4,9 @@ import matplotlib.dates as mdates
 import csvfile
 
 # 전처리, 토큰화 된 테슬라 뉴스 데이터를 lda를 통해 추출한 토픽을 추출
-# 해당하는 토픽의 일자찾기 
-# 일자에 해당하는 거개량 찾기 
+# FVE : 
+# 각 토픽의 주식 거래량에 대한 설명 가능한 비율을 계산. (논문에서는 0.5% 이상의 FVE를 사용)
+# FVE 계산식: FVE = (해당 토픽의 거래량) / (전체 거래량)
 
 print("---------------------테슬라 뉴스 데이터 토픽별 날짜 추출 ------------------------")
 
@@ -14,18 +15,20 @@ newsFileName = 'tesla_news_topics'
 
 #테슬라 뉴스 dataFrame
 words_df = pd.DataFrame()
-words_df = csvfile.read_csv(fsilePath, newsFileName)
+words_df = csvfile.read_csv(fsilePath, newsFileName) 
 
 #datetime으로 타입 변환
 words_df['날짜'] = pd.to_datetime(words_df['날짜'])
 
 # 해당하는 토픽 
-top_topic = '엑'
+top_topic = '전기'
+keyword = 'Electric'
 
-# nouns_content에서 해당하는 토픽행만 추출
-filtered_rows = words_df[words_df['nouns_content'].str.contains(top_topic, case=False, na=False)]
+# pos_content 해당하는 토픽행만 추출
+filtered_rows = words_df[words_df['pos_content'].str.contains(top_topic, case=False, na=False)]
+print(filtered_rows[:5])
 
-# nouns_content에서 토픽에 해당한 날짜만 추출
+# pos_content 토픽에 해당한 날짜만 추출
 filtered_rows_date = filtered_rows['날짜'].dt.strftime('%Y-%m-%d').unique()
 #print(filtered_rows_date)
 
@@ -49,26 +52,26 @@ stocks_df['거래량'] = pd.to_numeric(stocks_df['거래량'].str.replace('M', '
 
 topics_date = filtered_rows_date.tolist()
 
-# nouns_content에서 해당하는 토픽행만 추출
+# pos_content 해당하는 토픽행만 추출
 filtered_stock_rows = stocks_df[stocks_df['날짜'].isin(topics_date)]
 
-# nouns_content에서 토픽에 해당한 날짜만 추출
+# pos_content 토픽에 해당한 날짜만 추출
 print(filtered_stock_rows[['날짜', '거래량']].shape)
 
 print("---------------------테슬라 전체 주식 거래량 그래프 ------------------------")
-# mergeFilePath = './Tesla_Project/data/merge/'
-# stockFileName = 'merge_stock_data'
+mergeFilePath = './Tesla_Project/data/merge/'
+stockFileName = 'merge_stock_data'
 
-# #테슬라 주식 dataFrame
-# stocks_df = pd.DataFrame()
-# stocks_df = csvfile.read_csv(mergeFilePath, stockFileName)
+#테슬라 주식 dataFrame
+stocks_df = pd.DataFrame()
+stocks_df = csvfile.read_csv(mergeFilePath, stockFileName)
 
-# #datetime으로 타입 변환
-# stocks_df['날짜'] = pd.to_datetime(stocks_df['날짜'])
-# #stocks_date = stocks_df['날짜'].sort_values(['날짜'], ascending=False )
+#datetime으로 타입 변환
+stocks_df['날짜'] = pd.to_datetime(stocks_df['날짜'])
+#stocks_date = stocks_df['날짜'].sort_values(['날짜'], ascending=False )
 
-# #거래량 M를 제외한 숫자로만 표기습을 진행함
-# stocks_df['거래량'] = pd.to_numeric(stocks_df['거래량'].str.replace('M', ''))
+#거래량 M를 제외한 숫자로만 표기습을 진행함
+stocks_df['거래량'] = pd.to_numeric(stocks_df['거래량'].str.replace('M', ''))
 
 
 print("---------------------해당하는 토픽 날짜의 주식 거래량 그래프 ------------------------")
@@ -77,16 +80,19 @@ fig_total = plt.figure(figsize=(12, 6)) # 차트 생성 및 사이즈 설정
 
 # 주식 거래량 전체 데이터 
 ax = fig_total.add_subplot(1, 1, 1)
-ax.bar(stocks_df['날짜'], stocks_df['거래량'], label='Stock Volume', color='black')
-ax.set_title('Volume for Topic : X', fontsize=20)
+ax.bar(stocks_df['날짜'], stocks_df['거래량'], label='Telsa Stock', color='#555555')
+#ax.set_title('Volume for Topic : X', fontsize=20)
+#그래프 제목
+ax.set_title(keyword, fontsize=20)
+
 ax.set_xlabel('Date', fontsize=14)
-ax.set_ylabel('News Volume', fontsize=14)
+ax.set_ylabel('Stock Volume', fontsize=14)
 ax.legend(fontsize=12, loc='best')
 
-# 해당하는 토픽 거래량 ex. 테슬라
+#해당하는 토픽 거래량 ex. 테슬라
 ax2 = ax.twinx()
-ax2.bar(filtered_stock_rows['날짜'], filtered_stock_rows['거래량'], label='Tesla Topic', color='red')
-ax2.set_ylabel('Tesla Topic', fontsize=14)
+ax2.bar(filtered_stock_rows['날짜'], filtered_stock_rows['거래량'], label='Topic', color='red')
+ax2.set_ylabel('News Volume', fontsize=14)
 
 # x 축의 날짜 형식 설정
 date_format = mdates.DateFormatter("%Y-%m")
@@ -100,7 +106,7 @@ ax.legend(lines, labels, fontsize=12, loc='upper left')
 ax2.legend(lines2, labels2, fontsize=12, loc='upper right')
 
 #그래프 파일 저장
-plt.savefig("./Tesla_Project/data/img/X.png", format = 'png')
+plt.savefig("./Tesla_Project/data/visualization/" + keyword + ".png", format = 'png')
 
 # 그래프 보기
 plt.tight_layout() 
